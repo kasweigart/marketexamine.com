faf0f1fe8e403d7b2320637812a113777a258f01
Kyle Sweigart <kasweigart@gmail.com>
2020-07-08 18:33:34 -0400

    server.js path change

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use("/contact", contactRouter);
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
